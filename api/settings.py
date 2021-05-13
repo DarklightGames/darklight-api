d7dbfa9c15c034c6eb3f1d6476cc49ff41725f29
shiftycats <mooncats@tutanota.com>
2021-05-13 11:27:39 +0000

    Hide the exposed key

--- api/settings.py
+++ api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wzq=89*bm790l(@3-6150s1o1bmnyf55cwxjh(xvq9m9pea+-c'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
